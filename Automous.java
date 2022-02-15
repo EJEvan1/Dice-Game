fe15dde6fb447de40c934b8e4c1b7d328afaf5e9
EJEvan1 <43522005+EJEvan1@users.noreply.github.com>
2022-02-15 18:14:46 +0000

    New stuff

--- Automous.java
+++ Automous.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.Random;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 class Automous {
+  Resize image = new Resize();
+  private static int countingRoll = 2;
   public static double gameSpeed = 1.0;
-  public static JFrame autoBoard;
+  public static JFrame autoBoard, rolls;
   private static JMenu menu, game, visual, gameSub;
+  private static JScrollPane scrollPane;
+  private static JTable table;
   private static JMenuItem m1,m2,m3,g1,g2, gs1, gs2,v1,v2;
-  private static JButton roll, startGame;
+  private static JButton rollButton, startGame;
   private static JLabel diceNum;
   private static boolean imageDis=true, test;
   public static void autoPlay(){
@@ -19,6 +27,7 @@ class Automous {
     labels();
     intalizeAction();
     autoBoard.setLayout(null);
+    autoBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     autoBoard.setVisible(true);
     diceDisplay(diceRoll(numOfSides));
   }
@@ -53,11 +62,13 @@ class Automous {
   private static void buttons(){
     //create buttons
     startGame = new JButton("Start");
+    rollButton = new JButton("Roll");
     //roll should have a image of a dice as its image. Might have to do some trouble shooting for this though.
-    roll = new JButton();
     //add tool tip text
     startGame.setToolTipText("Starts the game!");
+    rollButton.setToolTipText("Roll the dice");
     //set Bounds
+    rollButton.setBounds(182,90,75,50);
     startGame.setBounds(182, 90, 75, 50);
 
     //add to board
@@ -79,12 +90,15 @@ class Automous {
     m1.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
         System.out.println("Function should reset game board");
+        whiteBoard.deleteAllValues();
+        tableRenew();
+        countingRoll = 1;
         JOptionPane.showMessageDialog(null, "Game was reset. All varibles were cleared.");
       }
     });
     m3.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-        System.out.println("System was forced to exit by user");
+        System.out.println("Host Controller: System was forced to exit by user");
         System.exit(1);
       }
     });
@@ -97,9 +111,13 @@ class Automous {
     //start button action script
     startGame.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
-        System.out.println("This should start the game");
+        System.out.println("Host Controller: This should start the game");
         autoBoard.remove(startGame);
         autoBoard.add(diceNum);
+        autoBoard.add(rollButton);
+        table();
+        whiteBoard.addRoll(1, diceRoll(6) + 1);
+        tableRenew();
         autoBoard.repaint();
       }
     });
@@ -122,35 +140,62 @@ class Automous {
         autoBoard.repaint();
       }
     });
+    rollButton.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent e){
+        System.out.println("Host Controller: Rolling the dice");
+        int temp = diceRoll(6) + 1;
+        whiteBoard.addRoll(countingRoll, temp);
+        diceDisplay(temp);
+        countingRoll++;
+        tableRenew();
+      }
+    });
   }
 
   //Method to display different dice rolls
   private static void diceDisplay(int roll){
-    if (roll == 1){
+    if (roll == 1 && imageDis){
       System.out.println("An image of a dice with 1 would be displayed");
     }
-    else if (roll == 2){
+    else if (roll == 2 && imageDis){
       System.out.println("An image of a dice with 2 would be displayed");
     }
-    else if (roll == 3){
+    else if (roll == 3 && imageDis){
       System.out.println("An image of a dice with 3 would be displayed");
     }
-    else if (roll == 4){
+    else if (roll == 4 && imageDis){
       System.out.println("An image of a dice with 4 would be displayed");
     }
-    else if (roll == 5){
+    else if (roll == 5 && imageDis){
       System.out.println("An image of a dice with 5 would be displayed");
     }
-    else if (roll == 6){
+    else if (roll == 6 && imageDis){
       System.out.println("An image of a dice with 6 would be displayed");
     }
     else {
-      System.out.println("Error occured in diceDisplay Method");
+      System.out.println("Host Controller: Error occured in diceDisplay Method or images have been disabled");
     }
   }
 
   public static void table(){
-   // String[] columnNames = {"Roll #", "Value"};
-    
+   //String[][] columnNames = new String[2][2];
+  String[][] dataTypes = whiteBoard.getData();
+    String[] values = {"Roll", "Value"};
+    rolls = new JFrame("Rolls");
+    table = new JTable(dataTypes, values);
+    rolls.setSize(150, 100);
+    rolls.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    scrollPane = new JScrollPane(table);
+    rolls.add(scrollPane);
+    rolls.setVisible(true);
+  }
+  private static void tableRenew(){
+    String[][] dataTypes = whiteBoard.getData();
+    String[] columnNames = {"Roll", "Value"};
+    table = new JTable(dataTypes, columnNames);
+    rolls.remove(scrollPane);
+    scrollPane = new JScrollPane(table);
+    rolls.add(scrollPane);
+    rolls.repaint();
   }
 }
--- whiteBoard.java
+++ whiteBoard.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Arrays;
+
 //this method should be used to track the varibles used
-class whiteBoard{
+//First 1 dimension is used the Roll count example: dataRoll[0][0] = "Roll X" and dataRoll[0][1] = Actual data value
+class whiteBoard {
+  // String is set to a maxium 50 by 50 array size to avoid out of bounds
+  public static String[][] dataRoll = new String[50][2];
   private static final String BLACK = "\u001B[30m"; // BLACK
   private static final String RED = "\u001B[31m"; // RED
   private static final String GREEN = "\u001B[32m"; // GREEN
@@ -10,7 +15,95 @@ class whiteBoard{
   private static final String PURPLE = "\u001B[35m"; // PURPLE
   private static final String CYAN = "\u001B[36m"; // CYAN
   private static final String WHITE = "\u001B[37m"; // WHITE
-  private static final String ANSI_RESET = "\u001B[0m"; //RESET COLOR
+  private static final String ANSI_RESET = "\u001B[0m"; // RESET COLOR
   // ArrayList<int, String> test = new ArrayList<int, String>;
-  //test.add()
+  // test.add()
+
+  public static void main(String[] args) {
+    String[][] test = new String[99][99];
+    test[0][1] = "Test";
+    // test[0][] = "2";
+    test[1][0] = "Roll 2";
+    test[1][1] = "23";
+    int counter = 0;
+    try {
+      for (int i = 0; i < test.length; i++) {
+        for (int j = 0; j < test.length; j++) {
+          if (test[i][j] != null && counter < 2) {
+            System.out.print(test[i][j] + " ");
+            counter++;
+          } else if (test[i][j] != null) {
+            System.out.println("\n");
+            System.out.print(test[i][j] + " ");
+            counter = 0;
+          }
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static int getValueInt(int a, int b) {
+    return Integer.valueOf(dataRoll[a][b]);
+  }
+
+  public static String getString(int a, int b) {
+    return dataRoll[a][b];
+  }
+
+  public static void deleteAllValues() {
+    dataRoll = new String[50][2];
+    System.out.println("Host Controller: dataRoll's values were all set to null");
+  }
+
+  public static void addRoll(int rollNumber, int value) {
+    if (String.valueOf(rollNumber) == null) {
+      for (int i = 0; i < dataRoll.length; i++) {
+        if (dataRoll[i][0] == null) {
+          dataRoll[i][0] = "Roll " + (i + 1);
+        }
+      }
+    } else {
+      dataRoll[rollNumber - 1][0] = "Roll " + rollNumber;
+    }
+    dataRoll[rollNumber - 1][1] = String.valueOf(value);
+  }
+  public static String[][] getData(){
+    String[][] dataRollCompressed = Arrays.copyOf(dataRoll, countTillNull());
+    //this a primative way of doing it
+    return dataRollCompressed;
+  }
+  private static int countTillNull(){
+    for (int i = 0; i < dataRoll.length; i++){
+      if (dataRoll[i][0] == null && dataRoll[i][1] == null){
+        return i;
+      }
+    }
+    return 0;
+  }
+  public static void addRoll(String rollNumber, int value) {
+    int i = 0;
+    if (rollNumber == null) {
+      for (i = 0; i < dataRoll.length; i++) {
+        if (dataRoll[i][0] == null) {
+          dataRoll[i][0] = "Roll " + (i + 1);
+        }
+      }
+      rollNumber = String.valueOf(i) + 1;
+    } else {
+      dataRoll[Integer.valueOf(rollNumber) - 1][0] = "Roll " + rollNumber;
+    }
+    dataRoll[i][1] = String.valueOf(value);
+  }
+  public static boolean checkIfAlreadyInUse(int toCheckfor){
+    for (int i = 0; i < dataRoll.length; i++){
+      for (int j = 0; j < dataRoll.length; j++){
+        if (dataRoll[i][j] == String.valueOf(toCheckfor)){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
